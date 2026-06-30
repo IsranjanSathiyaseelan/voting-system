fb0f4acbe460fa70f42eeb8315ec66f7814285ef
IsranjanSathiyaseelan <isranjansathiyaseelan@gmail.com>
2026-06-30 14:51:45 +0530

    added useAuth, authService, auth files

---
+++ client/src/hooks/useAuth.ts
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+
+import { AuthContext } from "../context/AuthContext";
+import type { AuthContextType } from "../context/AuthContext";
+
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be inside AuthProvider");
+  }
+
+  return context;
+};
